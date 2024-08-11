2ca8ec95306abd1a3b079411edeb5d718fb5f91c
dkvaghela25 <dkvaghela724@gmail.com>
2024-08-11 15:28:59 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function toggle(id) {
     let elm = document.getElementById(id).nextElementSibling
-    // let elm = document.querySelector(".que").nextElementSibling
     elm.classList.toggle("visible")
 }
 
